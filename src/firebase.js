bf89d35025ac89749e32ef94c7effde1203ad8ef
syed khizaruddin <syedkhizaruddin@syeds-MacBook-Air.local>
2021-12-23 09:50:57 +0530

    header amazon clone finished

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDH8nXUgr7G5DLo2A1EpmruGrTQeSJST1w",
+    authDomain: "amz-clone-e4590.firebaseapp.com",
+    projectId: "amz-clone-e4590",
+    storageBucket: "amz-clone-e4590.appspot.com",
+    messagingSenderId: "349920048799",
+    appId: "1:349920048799:web:1b004f27c955bffa22d4aa"
+  };
